272726e33b7ef9d1473aa80f4765614144e8eb1d
Don Carderms <donphilocardermssouffrant@gmail.com>
2023-01-22 09:35:05 -0300

    arquivos iniciais

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+const btSpeak = document.querySelector('.bt-speak');
+const btStop = document.querySelector('.bt-stop');
+const speedRange = document.querySelector('.speed-range')
+const speedView = document.querySelector('.value-speed-view')
+
+speedRange.oninput = function  () {
+    speedView.textContent = this.value;
+}
+
+
+const utterance = new SpeechSynthesisUtterance();
+
+btSpeak.addEventListener('click', (e) => {
+    speak()
+    
+})
+
+btStop.addEventListener('click', (e) =>{
+    stop()
+})
+
+utterance.lang = "pt-BR";
+
+
+function speak() {
+    utterance.rate = speedRange.value;
+	speechSynthesis.speak(utterance);
+}
+
+function stop() {
+  speechSynthesis.cancel();
+}
+
+function setText(event) {
+	utterance.text = event.target.innerText;
+}
